b8087282ffeddf24439c1e21835a66631d940fb5
Navein Austin Fernandes <naveinaustin@gmail.com>
2022-03-26 23:40:55 +0530

    The following were implemented: 1) Customer Application (Spring Boot), Entity, Controller, Service, Repository 2) banner.txt 3) Connecting App to DB 4) Docker compose to get a Postgress and PG admin containers up

---
+++ austinservices/customer/src/main/java/com/naveinaustin/customer/CustomerService.java
@@ -0,0 +1,21 @@
+package com.naveinaustin.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public void registerCustomer(CustomerRegistrationRequest customerRequest) {
+        Customer customer = Customer.builder()
+                .firstName(customerRequest.getFirstName())
+                .lastName(customerRequest.getLastName())
+                .email(customerRequest.getEmail())
+                .build();
+
+        customerRepository.save(customer);
+    }
+}
